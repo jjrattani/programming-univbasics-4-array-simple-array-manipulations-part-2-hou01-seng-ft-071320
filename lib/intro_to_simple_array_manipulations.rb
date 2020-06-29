cec732a9f79be91e7399b02a6667b5af0a97f933
Jessica  Rattani <jjrattani@gmail.com>
2020-06-29 23:51:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,18 @@ def using_concat(array,array1)
 end
 
 def using_insert(array,array1)
-  array.insert(array1)
+  array.insert(4,array1)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
